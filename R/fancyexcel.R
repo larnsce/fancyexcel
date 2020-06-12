4cce4a6a67910b49c23eca496bddc1024741d9ef
larnsce <lars@lse.de>
2020-06-12 11:31:24 +0200

    added source argument to function

--- R/fancyexcel.R
+++ R/fancyexcel.R
@@ -4,6 +4,7 @@
 #'
 #' @param data A dataframe
 #' @param title Title for the sheet to be added
+#' @param source Quellenangabe hinzufuegen
 #'
 #' @return
 #' @export
@@ -12,12 +13,12 @@
 #' df <- datasets::swiss
 #' fancy_excel(head(df), title = "My fancy title")
 
-fancy_excel <- function(data , title = "Title") {
+fancy_excel <- function(data , title = "Title", source = "Quelle:") {
 
   # Workbook erstellen
   wb <- openxlsx::createWorkbook("workbook")
 
-  datenbereich = 2
+  datenbereich = 3
 
   ## Add worksheet
   openxlsx::addWorksheet(wb, "data")
@@ -34,6 +35,14 @@ fancy_excel <- function(data , title = "Title") {
                       sheet = 1,
                       x = data,
                       startRow = datenbereich
+
+  )
+
+  #Quelle hinzufügen
+  openxlsx::writeData(wb,
+                      sheet = 1,
+                      x = source,
+                      startRow = 2
 
   )
 
