0f842a6bc4f8571466620bd1f2ab02ae3d43045e
larnsce <lars@lse.de>
2020-06-12 10:41:44 +0200

    initial commit. package documented. ran through 4 steps.

---
+++ R/fancyexcel.R
@@ -0,0 +1,43 @@
+#' Add data to an Excel file
+#'
+#' @param x A data frame
+#'
+#' @examples
+#' df <- dataset::swiss
+#' fancy_excel(head(df))
+#'
+#'
+
+fancy_excel <- function(data , title="Title") {
+
+  # Workbook erstellen
+  wb <- openxlsx::createWorkbook("workbook")
+
+  datenbereich = 2
+
+  ## Add worksheet
+  openxlsx::addWorksheet(wb,"data")
+
+  #Titel hinzufügen
+  openxlsx::writeData(wb
+                      ,sheet = 1
+                      ,title
+                      ,startRow = 1
+  )
+
+  #Daten abfüllen
+  openxlsx::writeData(wb
+                      ,sheet = 1
+                      ,data
+                      ,startRow = datenbereich
+
+  )
+
+  #speichere Workbook ab
+  openxlsx::saveWorkbook(wb,"example.xlsx")
+
+
+}
+
+
+fancy_excel(head(mtcars))
